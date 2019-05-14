1db1f8606bf402f8782b82c9e053eda7d3873466
Adrian <adrian@gmail.com>
2019-05-14 13:37:54 +0200

    Mi aplicación Django Girls, primer commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Adrián\Desktop\comienzo\sge\django\prueba2\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
